34a5a2d154db10a480c5c32456993198728d74c1
varunhallur <97945091+varunhallur@users.noreply.github.com>
2022-09-18 11:51:21 +0530

    Update Class1.py

--- Class1.py
+++ Class1.py
@@ -1,4 +1,12 @@
+print("Good Evening All")
 # """creating a variable"""
 a="Hello Everyone" #-> string
 print(a)
 print(type(a))
+num1=3387 #Integer
+num2=3.47 #float
+print(type(num2))
+print(type(num1))
+sum=num1+num2
+print(sum)
+
